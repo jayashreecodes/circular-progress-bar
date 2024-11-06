4387b09a34f7e2a9d9b4b52de206e67587f88e1b
jayashreecodes <jayashreejayashree05613@gmail.com>
2024-11-06 17:57:26 +0530

    Add files via upload
    A **circular progress bar** in GitHub visually represents the completion status of a task, such as a CI/CD pipeline or workflow, using a circular ring that fills as the task progresses.

---
+++ circular progress bar project/script.js
@@ -0,0 +1,13 @@
+let number= document.getElementById("number");
+let count=0;
+
+
+  setInterval(()=>{
+      if(count==100){
+        clearInterval();
+      }
+      else{
+        count+=1;
+        number.innerHTML=`${count}%`;      
+      }
+  },87);
